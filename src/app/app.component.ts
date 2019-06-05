3f7631ce70420fc3fcd3c758964d540db17367eb
Anand <anandchakru@gmail.com>
2019-06-05 15:59:35 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'neg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'nivite-eg1';
+}
